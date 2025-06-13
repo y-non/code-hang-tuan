6e93f4845cbf2fd8c5f50a2cb1070a695ba8201a
yonnon <trankhoadev@gmail.com>
2025-06-13 20:37:31 +0700

    upload bai js tuan 3

---
+++ Tuan 3/Buoi2/main.js
@@ -0,0 +1,28 @@
+// 1. let: biến cục bộ
+let a = "hello world";
+
+// 2. var: biến toàn cục
+var b = 15;
+
+// 3. const: biến hằng
+const c = 25;
+
+/* 
+    Kiểu giá trị:
+    - string
+    - number
+    - boolean
+    - array
+    - object
+    - null và undefined
+*/
+// ""
+let x = null;
+let d = true;
+// 0, 1, 2
+let arr = [8, 9, 10];
+let obj = {
+  ten: "khoa",
+  tuoi: 18,
+};
+console.log(x);
