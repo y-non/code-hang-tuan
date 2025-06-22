2b419ca4a88dcdcd8929ca1957575682e9d89865
yonnon <trankhoadev@gmail.com>
2025-06-22 09:55:44 +0700

    update btvn tuan 4 bai 2

--- Tuan4/Buoi2/BTVN/main.js
+++ Tuan4/Buoi2/BTVN/main.js
@@ -1,15 +1,31 @@
-function demoFunc() {
-  //lấy giá trị input người dùng nhập
-  let myText = document.getElementById("myText").value;
+function nhapTen() {
+  let nameInput = document.getElementById("myText").value;
 
-  //tạo li
-  let itemCon = document.createElement("li");
-  // gán giá trị input cho li
-  itemCon.textContent = myText;
-
-  //ánh xạ đến ul
   let danhSach = document.getElementById("danhSach");
+  let itemCon = document.createElement("li");
+  itemCon.textContent = nameInput;
+  itemCon.classList.add("li-child");
 
-  //thêm con cho ul
   danhSach.appendChild(itemCon);
+
+  let deleteBtn = document.createElement("button");
+  deleteBtn.className = "btn btn-danger";
+  deleteBtn.textContent = "Xóa";
+
+  deleteBtn.onclick = function () {
+    danhSach.removeChild(itemCon);
+  };
+
+  itemCon.appendChild(deleteBtn);
+
+  document.getElementById("myText").value = "";
+}
+
+function xoaTatCa() {
+  let tatCaLi = document.querySelectorAll("li.li-child");
+
+  let danhSach = document.getElementById("danhSach");
+  for (let i = 0; i < tatCaLi.length; i++) {
+    danhSach.removeChild(tatCaLi[i]);
+  }
 }
